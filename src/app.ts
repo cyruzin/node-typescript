5a653506687738c31ec009dd8d91d6b3d43c2554
Cyro Dubeux <xorycx@gmail.com>
2020-03-16 13:17:00 -0300

    Added husky to the project

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import morgan from 'morgan'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.listen(3000, () => console.log('Listening on port: 3000'));
+app.use(cors(), morgan('dev'))
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from the server!' })
+})
+
+app.listen(3000, () => console.log('Listening on port: 3000'))
